b6aac7aa40c968157f1941484a670082d891c8a3
LODYGENSKY Oleg <lodygens@lal.in2p3.fr>
2016-10-17 16:40:25 +0200

    use strict syntax

--- misc/xwserver2.js
+++ misc/xwserver2.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
  *
  * Copyrights     : CNRS
@@ -41,6 +43,7 @@
  * This cancels background color
  * @param thediv is the div to reset background color to
  */
+
 function resetBgColor(thediv)
 {
   if (thediv) {
