0c98069c808b8eaffc08192e4a33d09caba72b98
LODYGENSKY Oleg <lodygens@lal.in2p3.fr>
2016-10-21 11:15:32 +0200

    code clean up

--- src/xtremweb/common/GroupInterface.java
+++ src/xtremweb/common/GroupInterface.java
@@ -3,7 +3,7 @@
  * Author         : Oleg Lodygensky
  * Acknowledgment : XtremWeb-HEP is based on XtremWeb 1.8.0 by inria : http://www.xtremweb.net/
  * Web            : http://www.xtremweb-hep.org
- * 
+ *
  *      This file is part of XtremWeb-HEP.
  *
  *    XtremWeb-HEP is free software: you can redistribute it and/or modify
@@ -55,10 +55,15 @@ public final class GroupInterface extends xtremweb.common.Table {
 	/**
 	 * This is the database table name This was stored in
 	 * xtremweb.dispatcher.Group
-	 * 
+	 *
 	 * @since 9.0.0
 	 */
-	public static final String TABLENAME = ("groups");
+	public static final String TABLENAME = "groups";
+
+	/**
+	 * This is the XML tag
+	 */
+	public static final String THISTAG = "group";
 
 	public enum Columns implements XWBaseColumn {
 
@@ -69,7 +74,7 @@ public final class GroupInterface extends xtremweb.common.Table {
 			/**
 			 * This creates an object from String representation for this column
 			 * value This cleans the parameter to ensure SQL compliance
-			 * 
+			 *
 			 * @param v
 			 *            the String representation
 			 * @return a Boolean representing the column value
@@ -83,11 +88,12 @@ public final class GroupInterface extends xtremweb.common.Table {
 			}
 		},
 		/**
-		 * This is the column index of the UID of the associated session, if any<br />
+		 * This is the column index of the UID of the associated session, if any
+		 * <br />
 		 */
 		SESSIONUID {
 			@Override
-			public UID fromString(String v) throws URISyntaxException {
+			public UID fromString(final String v) throws URISyntaxException {
 				return new UID(v);
 			}
 		};
@@ -95,7 +101,7 @@ public final class GroupInterface extends xtremweb.common.Table {
 		/**
 		 * This is the index based on ordinal so that the first value is
 		 * TableColumns + 1
-		 * 
+		 *
 		 * @see xtremweb.common#TableColumns
 		 * @see Enum#ordinal()
 		 * @since 8.2.0
@@ -105,7 +111,7 @@ public final class GroupInterface extends xtremweb.common.Table {
 		/**
 		 * This constructor sets the ord member as ord = this.ordinal +
 		 * TableColumns.SIZE
-		 * 
+		 *
 		 * @since 8.2.0
 		 */
 		Columns() {
@@ -114,49 +120,50 @@ public final class GroupInterface extends xtremweb.common.Table {
 
 		/**
 		 * This retrieves the index based ordinal
-		 * 
+		 *
 		 * @return the index based ordinal
 		 * @since 8.2.0
 		 */
+		@Override
 		public int getOrdinal() {
 			return ord;
 		}
 
 		/**
 		 * This creates a new object from String for the given column
-		 * 
+		 *
 		 * @param v
 		 *            the String representation
 		 * @return the object representing the column
 		 * @throws Exception
 		 *             is thrown on instantiation error
 		 */
-		public Object fromString(String v) throws Exception {
+		@Override
+		public Object fromString(final String v) throws Exception {
 			throw new IOException("fromString() not implemented : " + this);
 		}
 
 		/**
 		 * This creates a new object from SQL result set
-		 * 
+		 *
 		 * @param rs
 		 *            is the SQL restul set
 		 * @return the object representing the column
 		 * @throws Exception
 		 *             is thrown on instantiation error
 		 */
-		public final Object fromResultSet(ResultSet rs) throws Exception {
+		public final Object fromResultSet(final ResultSet rs) throws Exception {
 			return this.fromString(rs.getString(this.toString()));
 		}
 
 		/**
 		 * This retrieves an Columns from its integer value
-		 * 
+		 *
 		 * @param v
 		 *            is the integer value of the Columns
 		 * @return an Columns
 		 */
-		public static XWBaseColumn fromInt(int v)
-				throws IndexOutOfBoundsException {
+		public static XWBaseColumn fromInt(final int v) throws IndexOutOfBoundsException {
 			try {
 				return TableColumns.fromInt(v);
 			} catch (final Exception e) {
@@ -179,7 +186,7 @@ public final class GroupInterface extends xtremweb.common.Table {
 	 * This retrieves column label from enum Columns. This takes cares of this
 	 * version. If this version is null, this version is prior to 5.8.0. Before
 	 * 5.8.0, OWNERUID was known as CLIENTUID and ACCESSRIGHTS did not exist.
-	 * 
+	 *
 	 * @param i
 	 *            is an ordinal of an Columns
 	 * @since 5.8.0
@@ -188,18 +195,13 @@ public final class GroupInterface extends xtremweb.common.Table {
 	 *         &amp;&amp; (i == OWNERUID.ordinal())); column label otherwise
 	 */
 	@Override
-	public String getColumnLabel(int i) throws IndexOutOfBoundsException {
+	public String getColumnLabel(final int i) throws IndexOutOfBoundsException {
 		try {
 			return TableColumns.fromInt(i).toString();
 		} catch (final Exception e) {
 		}
 		return Columns.fromInt(i).toString();
 	}
-
-	/**
-	 * This is the XML tag
-	 */
-	public static final String THISTAG = "group";
 
 	/**
 	 * This is the default constructor
@@ -211,48 +213,46 @@ public final class GroupInterface extends xtremweb.common.Table {
 		setAttributeLength(ENUMSIZE);
 
 		setAccessRights(XWAccessRights.USERALL);
-		setShortIndexes(new int[] { TableColumns.UID.getOrdinal(),
-				Columns.NAME.getOrdinal() });
+		setShortIndexes(new int[] { TableColumns.UID.getOrdinal(), Columns.NAME.getOrdinal() });
 	}
 
 	/**
 	 * This creates a new object that will be retreived with a complex SQL
 	 * request
-	 * 
+	 *
 	 * @since 9.0.0
 	 */
-	public GroupInterface(SQLRequest r) {
+	public GroupInterface(final SQLRequest r) {
 		this();
 		setRequest(r);
 	}
 
 	/**
 	 * This constructs an object from DB
-	 * 
+	 *
 	 * @param rs
 	 *            is an SQL request result
 	 * @exception IOException
 	 */
-	public GroupInterface(ResultSet rs) throws IOException {
+	public GroupInterface(final ResultSet rs) throws IOException {
 		this();
 		fill(rs);
 	}
 
 	/**
 	 * This fills columns from DB
-	 * 
+	 *
 	 * @since 9.0.0
 	 * @param rs
 	 *            is the SQL data set
 	 * @throws IOException
 	 */
 	@Override
-	public void fill(ResultSet rs) throws IOException {
+	public void fill(final ResultSet rs) throws IOException {
 		try {
 			setUID((UID) TableColumns.UID.fromResultSet(rs));
 			setOwner((UID) TableColumns.OWNERUID.fromResultSet(rs));
-			setAccessRights((XWAccessRights) TableColumns.ACCESSRIGHTS
-					.fromResultSet(rs));
+			setAccessRights((XWAccessRights) TableColumns.ACCESSRIGHTS.fromResultSet(rs));
 			setName((String) Columns.NAME.fromResultSet(rs));
 		} catch (final Exception e) {
 			throw new IOException(e.toString());
@@ -265,36 +265,36 @@ public final class GroupInterface extends xtremweb.common.Table {
 
 	/**
 	 * This calls this(StreamIO.stream(input));
-	 * 
+	 *
 	 * @param input
 	 *            is a String containing an XML representation
 	 */
-	public GroupInterface(String input) throws IOException, SAXException {
+	public GroupInterface(final String input) throws IOException, SAXException {
 		this(StreamIO.stream(input));
 	}
 
 	/**
 	 * This constructs a new object from an XML file
-	 * 
+	 *
 	 * @param f
 	 *            is the XML file
 	 * @see #GroupInterface(InputStream)
 	 */
-	public GroupInterface(File f) throws IOException, SAXException {
+	public GroupInterface(final File f) throws IOException, SAXException {
 		this(new FileInputStream(f));
 	}
 
 	/**
 	 * This constructs a new object from XML attributes received from input
 	 * stream
-	 * 
+	 *
 	 * @param input
 	 *            is the input stream
 	 * @see XMLReader#read(InputStream)
 	 * @throws IOException
 	 *             on XML error
 	 */
-	public GroupInterface(InputStream input) throws IOException, SAXException {
+	public GroupInterface(final InputStream input) throws IOException, SAXException {
 		this();
 		final XMLReader reader = new XMLReader(this);
 		try {
@@ -307,14 +307,14 @@ public final class GroupInterface extends xtremweb.common.Table {
 	/**
 	 * This constructs a new object from XML attributes received from input
 	 * stream
-	 * 
+	 *
 	 * @param attrs
 	 *            contains attributes XML representation
 	 * @see Table#fromXml(Attributes)
 	 * @throws IOException
 	 *             on XML error
 	 */
-	public GroupInterface(Attributes attrs) {
+	public GroupInterface(final Attributes attrs) {
 		this();
 		super.fromXml(attrs);
 	}
@@ -322,18 +322,18 @@ public final class GroupInterface extends xtremweb.common.Table {
 	/**
 	 * This is the default constructor
 	 */
-	public GroupInterface(UID uid) {
+	public GroupInterface(final UID uid) {
 		this();
 		setUID(uid);
 	}
 
 	/**
 	 * This updates this object from interface.
-	 * 
+	 *
 	 * @since 9.0.0
 	 */
 	@Override
-	public void updateInterface(Table gitf) throws IOException {
+	public void updateInterface(final Table gitf) throws IOException {
 		final GroupInterface itf = (GroupInterface) gitf;
 		if (itf.getName() != null) {
 			setName(itf.getName());
@@ -351,7 +351,7 @@ public final class GroupInterface extends xtremweb.common.Table {
 
 	/**
 	 * This gets an attribute
-	 * 
+	 *
 	 * @return this attribute, or null if not set
 	 */
 	public String getName() {
@@ -360,7 +360,7 @@ public final class GroupInterface extends xtremweb.common.Table {
 
 	/**
 	 * This gets an attribute
-	 * 
+	 *
 	 * @return this attribute, or null if not set
 	 * @exception IOException
 	 *                is thrown is attribute is nor well formed
@@ -376,7 +376,7 @@ public final class GroupInterface extends xtremweb.common.Table {
 	/**
 	 * This sets parameter value; this is called from
 	 * TableInterface#fromXml(Attributes)
-	 * 
+	 *
 	 * @param attribute
 	 *            is the name of the attribute to set
 	 * @param v
@@ -385,31 +385,30 @@ public final class GroupInterface extends xtremweb.common.Table {
 	 * @see Table#fromXml(Attributes)
 	 */
 	@Override
-	public final boolean setValue(String attribute, Object v)
-			throws IllegalArgumentException {
-		final String A = attribute.toUpperCase();
+	public final boolean setValue(final String attribute, final Object v) throws IllegalArgumentException {
+		final String uppercaseAttr = attribute.toUpperCase();
 		try {
-			return setValue(TableColumns.valueOf(A), v);
+			return setValue(TableColumns.valueOf(uppercaseAttr), v);
 		} catch (final Exception e) {
-			return setValue(Columns.valueOf(A), v);
+			return setValue(Columns.valueOf(uppercaseAttr), v);
 		}
 	}
 
 	/**
 	 * This sets the name
-	 * 
+	 *
 	 * @return true if name has changed, false otherwise
 	 */
-	public boolean setName(String v) {
+	public boolean setName(final String v) {
 		return setValue(Columns.NAME, v);
 	}
 
 	/**
 	 * This sets the session
-	 * 
+	 *
 	 * @return true if session has changed, false otherwise
 	 */
-	public boolean setSession(UID v) {
+	public boolean setSession(final UID v) {
 		return setValue(Columns.SESSIONUID, v);
 	}
 
@@ -420,7 +419,7 @@ public final class GroupInterface extends xtremweb.common.Table {
 	 * description and dumps it. <br />
 	 * Usage : java -cp xtremweb.jar xtremweb.common.GroupInterface [xmlFile]
 	 */
-	public static void main(String[] argv) {
+	public static void main(final String[] argv) {
 		try {
 			final GroupInterface itf = new GroupInterface();
 			itf.setUID(UID.getMyUid());
@@ -438,8 +437,7 @@ public final class GroupInterface extends xtremweb.common.Table {
 		} catch (final Exception e) {
 			final Logger logger = new Logger();
 			logger.exception("Usage : java -cp " + XWTools.JARFILENAME
-					+ " xtremweb.common.GroupInterface [anXMLDescriptionFile]",
-					e);
+					+ " xtremweb.common.GroupInterface [anXMLDescriptionFile]", e);
 		}
 	}
 }
