970fb5e1ad108597dc157bbb29add5dcbb4cd5cf
LODYGENSKY Oleg <lodygens@lal.in2p3.fr>
2016-10-21 11:19:22 +0200

    code clean up

--- src/xtremweb/common/Logger.java
+++ src/xtremweb/common/Logger.java
@@ -3,7 +3,7 @@
  * Author         : Oleg Lodygensky
  * Acknowledgment : XtremWeb-HEP is based on XtremWeb 1.8.0 by inria : http://www.xtremweb.net/
  * Web            : http://www.xtremweb-hep.org
- * 
+ *
  *      This file is part of XtremWeb-HEP.
  *
  *    XtremWeb-HEP is free software: you can redistribute it and/or modify
@@ -29,9 +29,9 @@ import java.util.Locale;
 
 /**
  * Logger.java
- * 
+ *
  * Created : 15 avril 2008
- * 
+ *
  * @author Oleg Lodygensky
  */
 
@@ -42,7 +42,7 @@ public class Logger {
 
 	/**
 	 * This retrieves the logging level
-	 * 
+	 *
 	 * @return the current logging level
 	 */
 	public LoggerLevel getLoggerLevel() {
@@ -51,11 +51,11 @@ public class Logger {
 
 	/**
 	 * This sets the logging level
-	 * 
+	 *
 	 * @param l
 	 *            is the new logging level
 	 */
-	public void setLoggerLevel(LoggerLevel l) {
+	public void setLoggerLevel(final LoggerLevel l) {
 		level = l;
 	}
 
@@ -67,8 +67,7 @@ public class Logger {
 	public Logger() {
 		level = LoggerLevel.INFO;
 		name = null;
-		final String levelstr = System.getProperty(XWPropertyDefs.LOGGERLEVEL
-				.toString());
+		final String levelstr = System.getProperty(XWPropertyDefs.LOGGERLEVEL.toString());
 		try {
 			level = LoggerLevel.valueOf(levelstr.toUpperCase());
 		} catch (final Exception e) {
@@ -78,7 +77,7 @@ public class Logger {
 	/**
 	 * This calls this() and sets the name to the parameter class name
 	 */
-	public Logger(Object o) {
+	public Logger(final Object o) {
 		this();
 		name = o.getClass().getName();
 	}
@@ -86,7 +85,7 @@ public class Logger {
 	/**
 	 * This calls this() and sets the logging level to the provided one
 	 */
-	public Logger(LoggerLevel l) {
+	public Logger(final LoggerLevel l) {
 		this();
 		level = l;
 	}
@@ -95,7 +94,7 @@ public class Logger {
 	 * This calls this() and sets the name and the logging level to the provided
 	 * ones
 	 */
-	public Logger(LoggerLevel l, Object o) {
+	public Logger(final LoggerLevel l, final Object o) {
 		this(o);
 		level = l;
 	}
@@ -103,31 +102,24 @@ public class Logger {
 	/**
 	 * This helps to format date : the format is "yyyy-MM-dd HH:mm:ss"
 	 */
-	private static final SimpleDateFormat LOG_DATE_FORMAT = new SimpleDateFormat(
-			"[dd/MMM/yyyy:HH:mm:ss Z]", Locale.US);
+	private static final SimpleDateFormat LOG_DATE_FORMAT = new SimpleDateFormat("[dd/MMM/yyyy:HH:mm:ss Z]", Locale.US);
 
 	/**
 	 * This logs out a message
 	 */
-	public void printLog(LoggerLevel levelmsg, String msg) {
-		String n = (name != null ? name + "_" : "")
-				+ Thread.currentThread().getName() + "_"
+	public void printLog(final LoggerLevel levelmsg, final String msg) {
+		final String n = (name != null ? name + "_" : "") + Thread.currentThread().getName() + "_"
 				+ Thread.currentThread().getId();
-		System.out.println(LOG_DATE_FORMAT.format(new Date()) + " [" + n + "] "
-				+ levelmsg + " : " + msg);
-		n = null;
+		System.out.println(LOG_DATE_FORMAT.format(new Date()) + " [" + n + "] " + levelmsg + " : " + msg);
 	}
 
 	/**
 	 * This logs out an exception
 	 */
-	public void exception(Exception e) {
-		String str = e.toString();
-		Throwable cause = e.getCause();
-		warn(str + " (" + (cause == null ? e.getMessage() : cause.toString())
-				+ ")");
-		cause = null;
-		str = null;
+	public void exception(final Exception e) {
+		final String str = e.toString();
+		final Throwable cause = e.getCause();
+		warn(str + " (" + (cause == null ? e.getMessage() : cause.toString()) + ")");
 		if (debug()) {
 			e.printStackTrace();
 		}
@@ -136,24 +128,24 @@ public class Logger {
 	/**
 	 * This logs out an exception
 	 */
-	public void exception(String msg, Exception e) {
+	public void exception(final String msg, final Exception e) {
 		error(msg);
 		exception(e);
 	}
 
 	/**
 	 * This tells whether logger level is set to FINEST
-	 * 
+	 *
 	 * @return true if logger level is set to FINEST
 	 */
 	public boolean finest() {
-		return (level.ordinal() <= LoggerLevel.FINEST.ordinal());
+		return level.ordinal() <= LoggerLevel.FINEST.ordinal();
 	}
 
 	/**
 	 * This logs out a FINEST message
 	 */
-	public void finest(String msg) {
+	public void finest(final String msg) {
 		if (finest()) {
 			printLog(LoggerLevel.FINEST, msg);
 		}
@@ -161,17 +153,17 @@ public class Logger {
 
 	/**
 	 * This tells whether logger level is set to DEBUG
-	 * 
+	 *
 	 * @return true if logger level is set to DEBUG
 	 */
 	public boolean debug() {
-		return (level.ordinal() <= LoggerLevel.DEBUG.ordinal());
+		return level.ordinal() <= LoggerLevel.DEBUG.ordinal();
 	}
 
 	/**
 	 * This logs out a DEBUG message
 	 */
-	public void debug(String msg) {
+	public void debug(final String msg) {
 		if (debug()) {
 			printLog(LoggerLevel.DEBUG, msg);
 		}
@@ -179,17 +171,17 @@ public class Logger {
 
 	/**
 	 * This tells whether logger level is set to INFO
-	 * 
+	 *
 	 * @return true if logger level is set to INFO
 	 */
 	public boolean info() {
-		return (level.ordinal() <= LoggerLevel.INFO.ordinal());
+		return level.ordinal() <= LoggerLevel.INFO.ordinal();
 	}
 
 	/**
 	 * This logs out an INFO message
 	 */
-	public void info(String msg) {
+	public void info(final String msg) {
 		if (info()) {
 			printLog(LoggerLevel.INFO, msg);
 		}
@@ -197,17 +189,17 @@ public class Logger {
 
 	/**
 	 * This tells whether logger level is set to CONFIG
-	 * 
+	 *
 	 * @return true if logger level is set to CONFIG
 	 */
 	public boolean config() {
-		return (level.ordinal() <= LoggerLevel.CONFIG.ordinal());
+		return level.ordinal() <= LoggerLevel.CONFIG.ordinal();
 	}
 
 	/**
 	 * This logs out an CONFIG message
 	 */
-	public void config(String msg) {
+	public void config(final String msg) {
 		if (config()) {
 			printLog(LoggerLevel.CONFIG, msg);
 		}
@@ -215,17 +207,17 @@ public class Logger {
 
 	/**
 	 * This tells whether logger level is set to WARN
-	 * 
+	 *
 	 * @return true if logger level is set to WARN
 	 */
 	public boolean warn() {
-		return (level.ordinal() <= LoggerLevel.WARN.ordinal());
+		return level.ordinal() <= LoggerLevel.WARN.ordinal();
 	}
 
 	/**
 	 * This logs out a WARN message
 	 */
-	public void warn(String msg) {
+	public void warn(final String msg) {
 		if (warn()) {
 			printLog(LoggerLevel.WARN, msg);
 		}
@@ -233,17 +225,17 @@ public class Logger {
 
 	/**
 	 * This tells whether logger level is set to ERROR
-	 * 
+	 *
 	 * @return true if logger level is set to ERROR
 	 */
 	public boolean error() {
-		return (level.ordinal() <= LoggerLevel.ERROR.ordinal());
+		return level.ordinal() <= LoggerLevel.ERROR.ordinal();
 	}
 
 	/**
 	 * This logs out an ERROR message
 	 */
-	public void error(String msg) {
+	public void error(final String msg) {
 		if (error()) {
 			printLog(LoggerLevel.ERROR, msg);
 		}
@@ -252,7 +244,7 @@ public class Logger {
 	/**
 	 * This logs out a FATAL message and exits
 	 */
-	public void fatal(String msg) {
+	public void fatal(final String msg) {
 		printLog(LoggerLevel.FATAL, msg);
 		System.exit(XWReturnCode.FATAL.ordinal());
 	}
