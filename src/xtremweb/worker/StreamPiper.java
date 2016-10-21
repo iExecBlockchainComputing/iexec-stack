cb9e81f1cb7ddd38a2bb9ec4b2dce57abb23cc18
LODYGENSKY Oleg <lodygens@lal.in2p3.fr>
2016-10-21 12:11:18 +0200

    code clean up

--- src/xtremweb/worker/StreamPiper.java
+++ src/xtremweb/worker/StreamPiper.java
@@ -3,7 +3,7 @@
  * Author         : Oleg Lodygensky
  * Acknowledgment : XtremWeb-HEP is based on XtremWeb 1.8.0 by inria : http://www.xtremweb.net/
  * Web            : http://www.xtremweb-hep.org
- * 
+ *
  *      This file is part of XtremWeb-HEP.
  *
  *    XtremWeb-HEP is free software: you can redistribute it and/or modify
@@ -37,7 +37,7 @@ public class StreamPiper implements Runnable {
 	private final BufferedWriter output;
 	private boolean canRun = true;
 
-	public StreamPiper(InputStream in, OutputStream out) {
+	public StreamPiper(final InputStream in, final OutputStream out) {
 
 		input = new BufferedReader(new InputStreamReader(in));
 		output = new BufferedWriter(new OutputStreamWriter(out));
@@ -47,6 +47,7 @@ public class StreamPiper implements Runnable {
 		canRun = false;
 	}
 
+	@Override
 	public void run() {
 		try {
 			int byteRead = input.read();
