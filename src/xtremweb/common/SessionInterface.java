c163f5789374973fc71017048fc3d24da49ce7f2
LODYGENSKY Oleg <lodygens@lal.in2p3.fr>
2016-10-21 11:23:01 +0200

    code clean up

--- src/xtremweb/common/SessionInterface.java
+++ src/xtremweb/common/SessionInterface.java
@@ -3,7 +3,7 @@
  * Author         : Oleg Lodygensky
  * Acknowledgment : XtremWeb-HEP is based on XtremWeb 1.8.0 by inria : http://www.xtremweb.net/
  * Web            : http://www.xtremweb-hep.org
- * 
+ *
  *      This file is part of XtremWeb-HEP.
  *
  *    XtremWeb-HEP is free software: you can redistribute it and/or modify
@@ -54,7 +54,7 @@ public final class SessionInterface extends xtremweb.common.Table {
 	/**
 	 * This is the database table name This was stored in
 	 * xtremweb.dispatcher.Session
-	 * 
+	 *
 	 * @since 9.0.0
 	 */
 	public static final String TABLENAME = ("sessions");
@@ -75,7 +75,7 @@ public final class SessionInterface extends xtremweb.common.Table {
 			/**
 			 * This creates an object from String representation for this column
 			 * value This cleans the parameter to ensure SQL compliance
-			 * 
+			 *
 			 * @param v
 			 *            the String representation
 			 * @return a Boolean representing the column value
@@ -92,7 +92,7 @@ public final class SessionInterface extends xtremweb.common.Table {
 		/**
 		 * This is the index based on ordinal so that the first value is
 		 * TableColumns + 1
-		 * 
+		 *
 		 * @see xtremweb.common#TableColumns
 		 * @see Enum#ordinal()
 		 * @since 8.2.0
@@ -102,7 +102,7 @@ public final class SessionInterface extends xtremweb.common.Table {
 		/**
 		 * This constructor sets the ord member as ord = this.ordinal +
 		 * TableColumns.SIZE
-		 * 
+		 *
 		 * @since 8.2.0
 		 */
 		Columns() {
@@ -111,49 +111,50 @@ public final class SessionInterface extends xtremweb.common.Table {
 
 		/**
 		 * This retrieves the index based ordinal
-		 * 
+		 *
 		 * @return the index based ordinal
 		 * @since 8.2.0
 		 */
+		@Override
 		public int getOrdinal() {
 			return ord;
 		}
 
 		/**
 		 * This creates a new object from String for the given column
-		 * 
+		 *
 		 * @param v
 		 *            the String representation
 		 * @return v
 		 * @throws Exception
 		 *             is thrown on instantiation error
 		 */
-		public Object fromString(String v) throws Exception {
+		@Override
+		public Object fromString(final String v) throws Exception {
 			return v;
 		}
 
 		/**
 		 * This creates a new object from SQL result set
-		 * 
+		 *
 		 * @param rs
 		 *            is the SQL result set
 		 * @return the object representing the column
 		 * @throws Exception
 		 *             is thrown on instantiation error
 		 */
-		public final Object fromResultSet(ResultSet rs) throws Exception {
+		public final Object fromResultSet(final ResultSet rs) throws Exception {
 			return this.fromString(rs.getString(this.toString()));
 		}
 
 		/**
 		 * This retrieves an Columns from its integer value
-		 * 
+		 *
 		 * @param v
 		 *            is the integer value of the Columns
 		 * @return an Columns
 		 */
-		public static XWBaseColumn fromInt(int v)
-				throws IndexOutOfBoundsException {
+		public static XWBaseColumn fromInt(final int v) throws IndexOutOfBoundsException {
 			try {
 				return TableColumns.fromInt(v);
 			} catch (final Exception e) {
@@ -176,7 +177,7 @@ public final class SessionInterface extends xtremweb.common.Table {
 	 * This retrieves column label from enum Columns. This takes cares of this
 	 * version. If this version is null, this version is prior to 5.8.0. Before
 	 * 5.8.0, OWNERUID was known as CLIENTUID and ACCESSRIGHTS did not exist.
-	 * 
+	 *
 	 * @param i
 	 *            is an ordinal of an Columns
 	 * @since 5.8.0
@@ -185,7 +186,7 @@ public final class SessionInterface extends xtremweb.common.Table {
 	 *         &amp;&amp; (i == OWNERUID.ordinal())); column label otherwise
 	 */
 	@Override
-	public String getColumnLabel(int i) throws IndexOutOfBoundsException {
+	public String getColumnLabel(final int i) throws IndexOutOfBoundsException {
 		try {
 			return TableColumns.fromInt(i).toString();
 		} catch (final Exception e) {
@@ -203,38 +204,36 @@ public final class SessionInterface extends xtremweb.common.Table {
 		setAttributeLength(ENUMSIZE);
 
 		setAccessRights(XWAccessRights.USERALL);
-		setShortIndexes(new int[] { TableColumns.UID.getOrdinal(),
-				Columns.NAME.getOrdinal() });
+		setShortIndexes(new int[] { TableColumns.UID.getOrdinal(), Columns.NAME.getOrdinal() });
 	}
 
 	/**
 	 * This constructs an object from DB
-	 * 
+	 *
 	 * @param rs
 	 *            is an SQL request result
 	 * @exception IOException
 	 */
-	public SessionInterface(ResultSet rs) throws IOException {
+	public SessionInterface(final ResultSet rs) throws IOException {
 		this();
 		fill(rs);
 	}
 
 	/**
 	 * This fills columns from DB
-	 * 
+	 *
 	 * @since 9.0.0
 	 * @param rs
 	 *            is the SQL data set
 	 * @throws IOException
 	 */
 	@Override
-	public void fill(ResultSet rs) throws IOException {
+	public void fill(final ResultSet rs) throws IOException {
 
 		try {
 			setUID((UID) TableColumns.UID.fromResultSet(rs));
 			setOwner((UID) TableColumns.OWNERUID.fromResultSet(rs));
-			setAccessRights((XWAccessRights) TableColumns.ACCESSRIGHTS
-					.fromResultSet(rs));
+			setAccessRights((XWAccessRights) TableColumns.ACCESSRIGHTS.fromResultSet(rs));
 			setName((String) Columns.NAME.fromResultSet(rs));
 		} catch (final Exception e) {
 			throw new IOException(e.toString());
@@ -243,46 +242,46 @@ public final class SessionInterface extends xtremweb.common.Table {
 
 	/**
 	 * This calls this(StreamIO.stream(input));
-	 * 
+	 *
 	 * @param input
 	 *            is a String containing an XML representation
 	 */
-	public SessionInterface(String input) throws IOException, SAXException {
+	public SessionInterface(final String input) throws IOException, SAXException {
 		this(StreamIO.stream(input));
 	}
 
 	/**
 	 * This constructs a new object from an XML file
-	 * 
+	 *
 	 * @param f
 	 *            is the XML file
 	 * @see #SessionInterface(InputStream)
 	 */
-	public SessionInterface(File f) throws IOException, SAXException {
+	public SessionInterface(final File f) throws IOException, SAXException {
 		this(new FileInputStream(f));
 	}
 
 	/**
 	 * This creates a new object that will be retrieved with a complex SQL
 	 * request
-	 * 
+	 *
 	 * @since 9.0.0
 	 */
-	public SessionInterface(SQLRequest r) {
+	public SessionInterface(final SQLRequest r) {
 		this();
 		setRequest(r);
 	}
 
 	/**
 	 * This constructs a new object from input stream
-	 * 
+	 *
 	 * @param input
 	 *            is the input stream
 	 * @see XMLReader#read(InputStream)
 	 * @throws IOException
 	 *             on XML error
 	 */
-	public SessionInterface(InputStream input) throws IOException, SAXException {
+	public SessionInterface(final InputStream input) throws IOException, SAXException {
 		this();
 		final XMLReader reader = new XMLReader(this);
 		try {
@@ -295,14 +294,14 @@ public final class SessionInterface extends xtremweb.common.Table {
 	/**
 	 * This constructs a new object from XML attributes received from input
 	 * stream
-	 * 
+	 *
 	 * @param attrs
 	 *            contains attributes XML representation
 	 * @see Table#fromXml(Attributes)
 	 * @throws IOException
 	 *             on XML error
 	 */
-	public SessionInterface(Attributes attrs) {
+	public SessionInterface(final Attributes attrs) {
 		this();
 		super.fromXml(attrs);
 	}
@@ -311,7 +310,7 @@ public final class SessionInterface extends xtremweb.common.Table {
 	 * This updates this object from interface.
 	 */
 	@Override
-	public void updateInterface(Table sitf) throws IOException {
+	public void updateInterface(final Table sitf) throws IOException {
 		final SessionInterface itf = (SessionInterface) sitf;
 		if (itf.getName() != null) {
 			setName(itf.getName());
@@ -326,7 +325,7 @@ public final class SessionInterface extends xtremweb.common.Table {
 
 	/**
 	 * This gets an attribute
-	 * 
+	 *
 	 * @return this attribute, or null if not set
 	 */
 	public String getName() {
@@ -336,7 +335,7 @@ public final class SessionInterface extends xtremweb.common.Table {
 	/**
 	 * This sets parameter value; this is called from
 	 * TableInterface#fromXml(Attributes)
-	 * 
+	 *
 	 * @param attribute
 	 *            is the name of the attribute to set
 	 * @param v
@@ -345,8 +344,7 @@ public final class SessionInterface extends xtremweb.common.Table {
 	 * @see Table#fromXml(Attributes)
 	 */
 	@Override
-	public final boolean setValue(String attribute, Object v)
-			throws IllegalArgumentException {
+	public final boolean setValue(final String attribute, final Object v) throws IllegalArgumentException {
 		final String A = attribute.toUpperCase();
 		try {
 			return setValue(TableColumns.valueOf(A), v);
@@ -357,10 +355,10 @@ public final class SessionInterface extends xtremweb.common.Table {
 
 	/**
 	 * This sets the name
-	 * 
+	 *
 	 * @return true if name has changed, false otherwise
 	 */
-	public boolean setName(String v) {
+	public boolean setName(final String v) {
 		return setValue(Columns.NAME, v);
 	}
 
@@ -371,7 +369,7 @@ public final class SessionInterface extends xtremweb.common.Table {
 	 * XML description and dumps it. Usage : java -cp xtremweb.jar
 	 * xtremweb.common.SessionInterface [xmlFile]
 	 */
-	public static void main(String[] argv) {
+	public static void main(final String[] argv) {
 		try {
 			final SessionInterface itf = new SessionInterface();
 			itf.setUID(UID.getMyUid());
@@ -388,11 +386,8 @@ public final class SessionInterface extends xtremweb.common.Table {
 			writer.write(itf);
 		} catch (final Exception e) {
 			final Logger logger = new Logger();
-			logger.exception(
-					"Usage : java -cp "
-							+ XWTools.JARFILENAME
-							+ " xtremweb.common.SessionInterface [anXMLDescriptionFile]",
-					e);
+			logger.exception("Usage : java -cp " + XWTools.JARFILENAME
+					+ " xtremweb.common.SessionInterface [anXMLDescriptionFile]", e);
 		}
 	}
 }
