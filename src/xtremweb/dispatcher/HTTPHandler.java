b6d233911e29681929f5d153f2e06b5e98ae5a76
LODYGENSKY Oleg <lodygens@lal.in2p3.fr>
2016-11-09 10:48:33 +0100

    must close channel

--- src/xtremweb/dispatcher/HTTPHandler.java
+++ src/xtremweb/dispatcher/HTTPHandler.java
@@ -684,6 +684,7 @@ public class HTTPHandler extends xtremweb.dispatcher.CommHandler {
 		r.write(response);
 		response.getWriter().flush();
 		response.setStatus(HttpServletResponse.SC_OK);
+		close();
 	}
 
 	/**
@@ -699,6 +700,7 @@ public class HTTPHandler extends xtremweb.dispatcher.CommHandler {
 		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 		response.sendRedirect(res.getName());
 		baseRequest.setHandled(true);
+		close();
 	}
 
 	/**
@@ -1120,6 +1122,7 @@ public class HTTPHandler extends xtremweb.dispatcher.CommHandler {
 			user = null;
 			obj = null;
 			baseRequest.setHandled(true);
+			close();
 			notifyAll();
 		}
 	}
