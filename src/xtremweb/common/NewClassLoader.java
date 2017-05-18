8c5a1e7bb38a9738bebbda51fe4df3f23e83073b
LODYGENSKY Oleg <lodygens@lal.in2p3.fr>
2017-05-18 17:17:41 +0200

    code clean

--- src/xtremweb/common/NewClassLoader.java
+++ src/xtremweb/common/NewClassLoader.java
@@ -25,7 +25,6 @@ package xtremweb.common;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
