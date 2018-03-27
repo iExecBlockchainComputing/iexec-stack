867a8c7b26f79246504ed07edbae10b404389b34
LODYGENSKY Oleg <oleg.lodygensky@lal.in2p3.fr>
2018-03-27 09:24:43 +0200

    license apache 2

--- src/main/resources/scripts/job-submission-scripts/xw-submit-ls-url-amp.sh
+++ src/main/resources/scripts/job-submission-scripts/xw-submit-ls-url-amp.sh
@@ -7,7 +7,19 @@
 #                         at LAL, Univ Paris-Sud, IN2P3/CNRS, Orsay, France
 #  Acknowledgment : XtremWeb-HEP is based on XtremWeb 1.8.0 by INRIA
 #  Home page      : http://www.xtremweb-hep.org
-#  Licence        : GPL v3, see http://www.gnu.org/licenses/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 #=============================================================================
 XW_INPUT_DIR=/var/www/html/3g-bridge/
 BASE_URI="http://dev17-portal.cpc.wmin.ac.uk:8080/repository/download?appid=1117&filename="
