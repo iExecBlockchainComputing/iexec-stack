d34f3f05da3b8ac68136f96b1ae43ea57f365583
LODYGENSKY Oleg <oleg.lodygensky@lal.in2p3.fr>
2018-04-02 09:56:47 +0200

    copyright

--- src/main/resources/benchmark/database/scripts/apps.sql
+++ src/main/resources/benchmark/database/scripts/apps.sql
@@ -5,20 +5,17 @@
 -- 
 --       This file is part of XtremWeb-HEP.
 -- 
---     XtremWeb-HEP is free software: you can redistribute it and/or modify
---     it under the terms of the GNU General Public License as published by
---     the Free Software Foundation, either version 3 of the License, or
---     (at your option) any later version.
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--     http://www.apache.org/licenses/LICENSE-2.0
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 -- 
---     XtremWeb-HEP is distributed in the hope that it will be useful,
---     but WITHOUT ANY WARRANTY; without even the implied warranty of
---     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
---     GNU General Public License for more details.
--- 
---     You should have received a copy of the GNU General Public License
---     along with XtremWeb-HEP.  If not, see <http://www.gnu.org/licenses/>.
--- 
-
+SET GLOBAL sql_mode = '';
 create temporary table appsjobs (
        thedate    datetime,
        appname    varchar(254),
@@ -34,7 +31,7 @@ insert into appsjobs (thedate,appname,completeds,avgexec,proxieds)
   select date(completeddate),
        apps.name,
        count(*) as completed,
-       format(avg(UNIX_TIMESTAMP(completeddate)-UNIX_TIMESTAMP(laststartdate)),0) as avgexec,
+       avg(UNIX_TIMESTAMP(completeddate)-UNIX_TIMESTAMP(laststartdate)) as avgexec,
        count(works.userproxy) as proxieds
        from apps,works,tasks,hosts
        where works.appuid=apps.uid
