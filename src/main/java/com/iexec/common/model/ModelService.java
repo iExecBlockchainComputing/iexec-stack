5a3ba87d1dbaf74cabd54dec8c803e2150d79cae
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2018-05-11 11:33:57 +0200

    tuple2marketOrderModel converter

--- src/main/java/com/iexec/common/model/ModelService.java
+++ src/main/java/com/iexec/common/model/ModelService.java
@@ -1,17 +1,15 @@
 package com.iexec.common.model;
 
 
-import com.iexec.common.contracts.generated.*;
+import com.iexec.common.contracts.generated.App;
+import com.iexec.common.contracts.generated.Dataset;
+import com.iexec.common.contracts.generated.WorkOrder;
 import com.iexec.common.ethereum.CredentialsService;
-import com.iexec.common.ethereum.IexecConfigurationService;
 import com.iexec.common.ethereum.Web3jService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.web3j.tuples.generated.Tuple8;
 import org.web3j.tx.Contract;
 import org.web3j.tx.ManagedTransaction;
-
-import java.math.BigInteger;
 
 
 public class ModelService {
@@ -55,21 +53,6 @@ public class ModelService {
             WorkOrder workOrder = WorkOrder.load(
                     workOrderId, web3jService.getWeb3j(), credentialsService.getCredentials(), ManagedTransaction.GAS_PRICE, Contract.GAS_LIMIT);
             return new WorkOrderModel(workOrder.m_marketorderIdx().send(), workOrder.m_requester().send(), workOrder.m_app().send(), workOrder.m_dataset().send(), workOrder.m_workerpool().send(), workOrder.m_emitcost().send(), workOrder.m_params().send(), workOrder.m_callback().send(), workOrder.m_beneficiary().send());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public MarketOrderModel getMarketOrderIdx(BigInteger marketOrderIdx) {
-        try {
-            String iexecHubAddress = IexecConfigurationService.getInstance().getCommonConfiguration().getContractConfig().getIexecHubAddress();
-            IexecHub iexecHub = IexecHub.load(
-                    iexecHubAddress, web3jService.getWeb3j(), credentialsService.getCredentials(), ManagedTransaction.GAS_PRICE, Contract.GAS_LIMIT);
-            Marketplace marketplace = Marketplace.load(
-                    iexecHub.marketplaceAddress().send(), web3jService.getWeb3j(), credentialsService.getCredentials(), ManagedTransaction.GAS_PRICE, Contract.GAS_LIMIT);
-            Tuple8<BigInteger, BigInteger, BigInteger, BigInteger, BigInteger, BigInteger, String, String> marketOrder = marketplace.getMarketOrder(marketOrderIdx).send();
-            return new MarketOrderModel(marketOrder.getValue1(), marketOrder.getValue2(), marketOrder.getValue3(), marketOrder.getValue4(), marketOrder.getValue5(), marketOrder.getValue6(), marketOrder.getValue7(), marketOrder.getValue8());
         } catch (Exception e) {
             e.printStackTrace();
         }
