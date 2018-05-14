e874109bc7c89adbe9ba8ed443c87655880ebb42
Ugo Plouviez <up@iex.ec>
2018-05-14 14:32:10 +0200

    Add a cap to the number of max workers

--- src/main/resources/docker/startWorkersWithMultipleWallets.sh
+++ src/main/resources/docker/startWorkersWithMultipleWallets.sh
@@ -5,12 +5,18 @@
 # the wallets.list file is made up lines as follow:
 # WALLET_PATH WALLET_PASSWORD
 
+NB_CORES=`nproc --all`
+MAX_NB_WORKERS=$(($NB_CORES - 1))
 COUNTER=0
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
+    COUNTER=$[$COUNTER +1]
+    if [ $COUNTER -gt $MAX_NB_WORKERS ]; then
+      break
+    fi
+
     WALLET_PATH=`echo $line | awk -F ' ' '{print $1}'`
     WALLET_PASSWORD=`echo $line | awk -F ' ' '{print $2}'`
-    COUNTER=$[$COUNTER +1]
     WORKER_NAME=worker_$COUNTER
 
     ./startWorkerWithSingleWallet.sh $WORKER_NAME $WALLET_PATH $WALLET_PASSWORD
