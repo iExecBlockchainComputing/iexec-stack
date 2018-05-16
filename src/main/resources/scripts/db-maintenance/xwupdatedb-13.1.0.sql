bb3e905b56c9979f5d13083fab51b636bc018d88
LODYGENSKY Oleg <oleg.lodygensky@lal.in2p3.fr>
2018-05-16 10:25:26 +0200

    market order status

--- src/main/resources/scripts/db-maintenance/xwupdatedb-13.1.0.sql
+++ src/main/resources/scripts/db-maintenance/xwupdatedb-13.1.0.sql
@@ -72,6 +72,8 @@ create table if not exists  marketorders (
   remaining            bigint                   default 0          comment 'how many such orders left; this is calculated by the scheduler',
   workerpooladdr       varchar(254)   not null                     comment 'workerpool smart contract address',
   workerpoolowneraddr  varchar(254)   not null                     comment 'workerpool owner address',
+  statusId             tinyint unsigned  not null  default 255        comment 'Status Id. See common/XWStatus.java',
+  status               varchar(36)       not null  default 'NONE'     comment 'Status. see common/XWStatus.java',
 
   index  idx_catgoryid         (categoryid),
   index  idx_workerpooladdr    (workerpooladdr),
